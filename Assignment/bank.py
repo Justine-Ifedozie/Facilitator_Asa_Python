81366c0c909c6fc83caade8340b6cd0bad0f0daf
Justine-Ifedozie <ifedoziennamdi445@gmail.com>
2025-05-31 16:09:02 +0100

    Bank app

--- Assignment/bank.py
+++ Assignment/bank.py
@@ -1,48 +1,78 @@
-def create_account(name, phone, balance = 0.0):
-        user_name = name
-        user_phonenumber = phone
-        account[user_name, user_phonenumber, balance]
+accounts = []
+
+def create_account(name, phone, account_number, balance = 0.0):
+        account = [name, phone, account_number, balance]
         accounts.append(account)
-        return
+        return accounts
 
-def show_balance():
-        pass
+def deposit(name, amount):
+        for account in accounts:
+                if account[0] == name:
+                        account[3] += amount
+                        return account
+        
+def withdraw(name, amount):
+        for account in accounts:
+                if account[0] == name:
+                        account[3] = account[3] - amount
+                        return account
+        
+        
+def show_balance(name):
+        for account in accounts:
+                if account[0] == name:
+                        print(f'Your account balance is: ${account[3]}')
 
-def deposit():
-        pass
-        
-def withdraw():
-        pass
-        
-balance = 0
+def view_accounts():
+        print(accounts)
+
 is_running = True
-
 while is_running:
         
         welcome_text = """
 Welcome to Jstee bank!
+_______________________
 You will be required to
-provide the following details:
-1. Show Balance
-2. Deposit
+make a selection:
+1. Create account
+2. Deposit Funds
 3. Withdraw
-4. Exit
+4. Show balance
+5. View accounts in the bank
+0. Exit
+_______________________
                                 """
         print(welcome_text)
 
 
 #Driver Class
-accounts = []
+        import random
 
         customer_choice = int(input("Make a selection between 1 - 4: "))
         match customer_choice: 
                 case 1: 
-                        show_balance()
+                        user_name = input("Enter your name: ")
+                        user_phonenumber = input("Enter your phone number: ")
+                        user_account_number = random.randint(1230000000, 50000000000000)
+                        create_account(user_name, user_phonenumber, user_account_number, balance = 0.0)
+                        print("New Account Number: ", user_account_number)
+                        #print(accounts)
                 case 2: 
-                        deposit()
+                        account_name = input("Enter the name of the account you wish to make a deposit into: ")
+                        amount = int(input("Enter deposit amount: "))
+                        deposit(account_name, amount)
+                        #print(accounts)
                 case 3: 
-                        withdraw()
-                case 4:  
+                        account_user = input("Enter the name of the account you wish to withdraw from: ")
+                        withdraw_amount = int(input("Enter withdraw amount: "))
+                        withdraw(account_user, withdraw_amount)
+                        #print(accounts)
+                case 4: 
+                        account_name_2 = input("Enter the name of the account you wish to view its balance: ")
+                        show_balance(account_name_2)
+                case 5: 
+                        view_accounts()
+                case 0:  
                         is_running = False
                 case _: 
                         print("Invalid input")
