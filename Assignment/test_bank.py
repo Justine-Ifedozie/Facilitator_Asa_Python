fbf2e63f105af4707d17a49e6a2a8a8076349ea3
Justine-Ifedozie <ifedoziennamdi445@gmail.com>
2025-06-01 06:47:03 +0100

    Learning Python TDD

--- Assignment/test_bank.py
+++ Assignment/test_bank.py
@@ -1,12 +1,27 @@
-import bank
+from bank import create_account, deposit, withdraw, show_balance, view_accounts, accounts
 from unittest import TestCase
 
 class TestBank (TestCase):
-        def test_that_create_account_function_exists(self):
-                bank.create_account()
+        def setup(self):
+                accounts.clear()
 
-        def test_that_i_can_create_a_bank_account(self):
-                actual = 
+        def test_create_account(self):
+                new_account = create_account("Justine", "09021887133", 21345566788, 200)
+                self.assertEqual(len(accounts), 1)
+                self.assertEqual(new_account[0] [0], "Justine")
+                create_account("Esther", "07081726343", 2454553432322, 100.0)
+                self.assertEqual(len(accounts), 2)
+                self.assertIn(["Esther", "07081726343", 2454553432322, 100.0], accounts)
+
+        def test_deposit(self):
+                create_account("Esther", "07081726343", 2454553432322, 100.0)
+                updated_account = deposit("Esther", 50.0)
+                self.assertIsNotNone(updated_account)
+                self.assertEqual(updated_account[3], 150.0)
+
+
+
+
 
 
 
